f2647656044cf43efded5d1ea65414749c8210f0
Thor <thor@10.14.6>
2019-11-11 07:51:21 +0100

    edits

--- src/sprog/Dansk.java
+++ src/sprog/Dansk.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sprog;
-
-/**
- *
- * @author thor
- */
 public class Dansk {
 			public void printMenu() {
 			System.out.println("Jeg hedder Kurt");
